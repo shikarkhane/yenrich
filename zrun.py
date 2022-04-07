048381b5cfd6f1b69ef83ca26c07d5067839190b
Márton Borsos <borsos.marton95@gmail.com>
2022-04-07 13:03:00 +0200

    Removed unused code from zrun

--- zrun.py
+++ zrun.py
@@ -1,24 +1,6 @@
-from datetime import datetime, timedelta
-
-from flask import Response, session
-from flask.sessions import SecureCookieSessionInterface
-
 from wms import create_app
-from dashboard.common.date_utility import UTC_TIME_FORMAT
 
 app = create_app()
-
-session_cookie = SecureCookieSessionInterface().get_signing_serializer(app)
-
-
-@app.after_request
-def after_request(response: Response):
-    same_cookie = session_cookie.dumps(dict(session))
-    response.headers.add('Set-Cookie', f"session={same_cookie}; Secure; HttpOnly; SameSite=None; Path=/;"
-                                       f"Expires={(datetime.now() + timedelta(days=1)).strftime(UTC_TIME_FORMAT)};")
-    response.headers.set('Access-Control-Allow-Credentials', "true")
-    return response
-
 
 if __name__ == "__main__":
     app.run()
